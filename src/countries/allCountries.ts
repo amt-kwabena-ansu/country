4df33cee1c811505539b0f188bbe2029eb93fabd
kwabenakay <121056440+kayjob@users.noreply.github.com>
2023-03-17 20:36:06 +0000

    updated_countries

--- src/countries/allCountries.ts
+++ src/countries/allCountries.ts
@@ -6,8 +6,8 @@ export function setCountry(input:resultTypeCountry[]){
     allCountries.list=input
 }
 export function getCountry(countryName:string){
-    return allCountries.list.filter((country)=>(country.name==countryName))
+    return allCountries.list.filter((country)=>(country.name===countryName))
 }
 export function getNeighbourNames(codes:string[]){
-    return codes.map((code)=>(allCountries.list.filter((country)=>(country.alpha3Code==code)))[0].name)
+    return codes.map((code)=>(allCountries.list.filter((country)=>(country.alpha3Code===code)))[0].name)
 }
